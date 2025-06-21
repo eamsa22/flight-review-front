411298262e954a862a06ee8df0f21e6f272f5589
asmae <asmae.bouazza01@gmail.com>
2025-06-21 19:49:26 +0200

    add home page with two buttons linked to other components to submit review and view reviews's list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { ReviewsFormComponent } from './features/reviews-form/reviews-form.component';
+import { ReviewsListComponent } from './features/reviews-list/reviews-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'submit-review', component: ReviewsFormComponent},
+    { path: 'view-reviews', component: ReviewsListComponent },
+  ];
---
+++ src/app/features/reviews-form/reviews-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reviews-form',
+  imports: [],
+  templateUrl: './reviews-form.component.html',
+  styleUrl: './reviews-form.component.scss'
+})
+export class ReviewsFormComponent {
+
+}
